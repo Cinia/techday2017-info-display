4f6ebfa6ca8945847eb3b58a130cf6546b1fe6a5
Riku Lindblad <riku.lindblad@cinia.fi>
2017-10-24 17:41:57 +0300

    Disable example containers

--- front/src/index.php
+++ front/src/index.php
@@ -2,12 +2,9 @@
 $docker_localhost = "proxy:8080";
 
 $backends = array(
-"http://$docker_localhost/first/",
-"http://$docker_localhost/gotest/satiaisia",
 "http://$docker_localhost/boardgames/plays/latestgames",
 "http://$docker_localhost/boardgames/plays/statistics",
 "http://$docker_localhost/antell/",
-"http://$docker_localhost/second/page",
 "http://$docker_localhost/abc/",
 "http://$docker_localhost/now-listening/");
 
