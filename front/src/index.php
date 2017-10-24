5ce3abb02aec361aef60570797dc1da846367da4
Lasse Laaksonen <lasse.laaksonen@cinia.fi>
2017-10-24 16:09:13 +0300

    Fetches today's menu from Antell site.

--- front/src/index.php
+++ front/src/index.php
@@ -4,6 +4,7 @@ $docker_localhost = "proxy:8080";
 $backends = array( 
 "http://$docker_localhost/first/", 
 "http://$docker_localhost/gotest/satiaisia",
+"http://$docker_localhost/antell/",
 "http://$docker_localhost/second/page");
 
 if(!isset($_GET["index"]) || $_GET["index"] === "") {
