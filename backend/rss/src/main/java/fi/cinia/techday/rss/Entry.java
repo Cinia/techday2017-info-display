bfef28069902f78f079cf1667174cc3551fc8adb
Joni Kukko <joni.kukko@cinia.fi>
2017-10-24 18:07:31 +0300

    RSS reader backend

---
+++ backend/rss/src/main/java/fi/cinia/techday/rss/Entry.java
@@ -0,0 +1,29 @@
+package fi.cinia.techday.rss;
+
+import java.time.Instant;
+
+public class Entry {
+
+	private final String content;
+	private final Instant timestamp;
+	private final String title;
+
+	public Entry(Instant timestamp, String title, String content) {
+		this.timestamp = timestamp;
+		this.title = title;
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public Instant getTimestamp() {
+		return timestamp;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+}
