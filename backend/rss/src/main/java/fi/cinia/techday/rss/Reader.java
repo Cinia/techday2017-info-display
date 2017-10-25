3616915ad20a8f488661758aea25a86b7a361dff
Joni Kukko <joni.kukko@cinia.fi>
2017-10-25 21:03:33 +0300

    Added GET parameter count
    - Can be used to limit the count of entries

--- backend/rss/src/main/java/fi/cinia/techday/rss/Reader.java
+++ backend/rss/src/main/java/fi/cinia/techday/rss/Reader.java
@@ -2,9 +2,11 @@ package fi.cinia.techday.rss;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 
 import org.springframework.stereotype.Component;
 
+import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
@@ -17,5 +19,15 @@ public final class Reader {
         URL feedUrl = new URL(path);
         XmlReader reader = new XmlReader(feedUrl);
         return new SyndFeedInput().build(reader);
+    }
+
+    public SyndFeed read(String path, int count) throws IOException, IllegalArgumentException, FeedException {
+        SyndFeed feed = read(path);
+        List<SyndEntry> entries = feed.getEntries();
+        if (entries.size() > count) {
+            entries.subList(count, entries.size()).clear();
+            feed.setEntries(entries);
+        }
+        return feed;
     }
 }
