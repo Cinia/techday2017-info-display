da72b41508707e1cd7791da35874bc932a5e8ba1
Joni Kukko <joni.kukko@cinia.fi>
2017-10-25 19:30:35 +0300

    Some basic improvements
    - Static classes to spring managed beans
    - Container for feed info

--- backend/rss/src/main/java/fi/cinia/techday/rss/Converter.java
+++ backend/rss/src/main/java/fi/cinia/techday/rss/Converter.java
@@ -1,28 +1,41 @@
 package fi.cinia.techday.rss;
 
-import java.time.Instant;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
 
+import com.rometools.rome.feed.synd.SyndContent;
 import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
 
+@Component
 public final class Converter {
 
-    public static List<Entry> convert(List<SyndEntry> entries) {
-        return entries.stream().map(Converter::convert).collect(Collectors.toList());
+    public List<Entry> convert(List<SyndEntry> entries) {
+        return entries.stream().map(this::convert).collect(Collectors.toList());
     }
 
-    public static Entry convert(SyndEntry entry) {
+    public Entry convert(SyndEntry entry) {
         String title = entry.getTitle();
-        String html = entry.getContents().isEmpty() ? "" : entry.getContents().get(0).getValue();
-        String content = Jsoup.parse(html).text();
-        Instant timestamp = entry.getPublishedDate().toInstant();
-        return new Entry(timestamp, title, content);
+        String description = entry.getDescription() == null ? null : entry.getDescription().getValue();
+        Date date = entry.getPublishedDate();
+        List<String> contents = entry.getContents().stream().map(SyndContent::getValue).filter(Objects::nonNull)
+                .map(Jsoup::parse).map(Document::text).collect(Collectors.toList());
+
+        return new Entry.Builder().published(date).title(title).description(description).contents(contents).build();
     }
 
-    private Converter() {
-        throw new AssertionError("Static class");
+    public Feed convert(SyndFeed syndFeed) {
+        Date date = syndFeed.getPublishedDate();
+        String title = syndFeed.getTitle();
+        String description = syndFeed.getDescription();
+        List<Entry> entries = convert(syndFeed.getEntries());
+
+        return new Feed.Builder().published(date).title(title).description(description).entries(entries).build();
     }
 }
