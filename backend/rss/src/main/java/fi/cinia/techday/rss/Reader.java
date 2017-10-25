7910bdc005d3bc65bbae7303d7d46bca2160cf5e
Joni Kukko <joni.kukko@cinia.fi>
2017-10-25 19:30:35 +0300

    Some basic improvements
    - Static classes to spring managed beans
    - Container for feed info

--- backend/rss/src/main/java/fi/cinia/techday/rss/Reader.java
+++ backend/rss/src/main/java/fi/cinia/techday/rss/Reader.java
@@ -3,20 +3,19 @@ package fi.cinia.techday.rss;
 import java.io.IOException;
 import java.net.URL;
 
+import org.springframework.stereotype.Component;
+
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 
+@Component
 public final class Reader {
 
-    public static SyndFeed read(String path) throws IOException, IllegalArgumentException, FeedException {
+    public SyndFeed read(String path) throws IOException, IllegalArgumentException, FeedException {
         URL feedUrl = new URL(path);
         XmlReader reader = new XmlReader(feedUrl);
         return new SyndFeedInput().build(reader);
-    }
-
-    private Reader() {
-        throw new AssertionError("Static class");
     }
 }
